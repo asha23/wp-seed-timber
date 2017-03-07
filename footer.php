43447632c1cf6514520b2c0860b797c93144f53a
Ash Whiting <awhiting@thisispegasus.co.uk>
2017-03-07 10:15:28 +0000

    completed first stage refactoring

--- footer.php
+++
@@ -1,8 +0,0 @@
-<?php // footer ?>
-
-<?php get_template_part( 'views/common/main', 'footer' ); ?>
-
-<?php wp_footer(); ?>
-
-</body>
-</html>
--- header.php
+++ header.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-
-<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if (IE 9)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js ie9"><![endif]-->
-<!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
-
-<?php get_template_part('views/common/main', 'head'); ?>
-
-<body <?php body_class(); ?>>
-
-<?php get_template_part( 'views/common/main', 'analytics' ); ?>
-
-<?php // Header ?>
-
-<?php get_template_part( 'views/common/main', 'header' ); ?>
+<?php
+/*
+ * Third party plugins that hijack the theme will call wp_head() to get the header template.
+ * We use this to start our output buffer and render into the view/page-plugin.twig template in footer.php
+ */
+$GLOBALS['timberContext'] = Timber::get_context();
+ob_start();
